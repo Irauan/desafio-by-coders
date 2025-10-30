bb732aef2c26580605e09b07b29da320815d24ff
Irauan Lage <irauan.lage@gmail.com>
2025-10-30 18:03:13 -0300

    feat: Add transaction type metadata and unit tests
    Introduce `TransactionTypeMetadata` to categorize transaction types as entries or exits and calculate their sign. Include comprehensive unit tests to validate behavior, enhancing domain logic and maintainability.

---
+++ DesafioByCoders.Api.Tests.Units/Transactions/TransactionTypeMetadataTests.cs
@@ -0,0 +1,44 @@
+﻿using DesafioByCoders.Api.Transactions;
+
+namespace DesafioByCoders.Api.Tests.Units.Transactions;
+
+public class TransactionTypeMetadataTests
+{
+    [Theory]
+    [InlineData(TransactionType.Debit, true)]
+    [InlineData(TransactionType.Boleto, false)]
+    [InlineData(TransactionType.Financing, false)]
+    [InlineData(TransactionType.Credit, true)]
+    [InlineData(TransactionType.LoanReceipt, true)]
+    [InlineData(TransactionType.Sales, true)]
+    [InlineData(TransactionType.TedReceipt, true)]
+    [InlineData(TransactionType.DocReceipt, true)]
+    [InlineData(TransactionType.Rent, false)]
+    public void IsEntry_ValidTransactionTypes_ShouldReturnExpectedValue(TransactionType type, bool expectedIsEntry)
+    {
+        // Act
+        var isEntry = type.IsEntry();
+
+        // Assert
+        Assert.Equal(expectedIsEntry, isEntry);
+    }
+    
+    [Theory]
+    [InlineData(TransactionType.Debit, 1)]
+    [InlineData(TransactionType.Boleto, -1)]
+    [InlineData(TransactionType.Financing, -1)]
+    [InlineData(TransactionType.Credit, 1)]
+    [InlineData(TransactionType.LoanReceipt, 1)]
+    [InlineData(TransactionType.Sales, 1)]
+    [InlineData(TransactionType.TedReceipt, 1)]
+    [InlineData(TransactionType.DocReceipt, 1)]
+    [InlineData(TransactionType.Rent, -1)]
+    public void Sign_ValidaTransactionTypes_ShouldReturnExpectedValue(TransactionType type, int expectedSign)
+    {
+        // Act
+        var sign = type.Sign();
+
+        // Assert
+        Assert.Equal(expectedSign, sign);
+    }
+}
---
+++ DesafioByCoders.Api/Transactions/TransactionTypeMetadata.cs
@@ -0,0 +1,17 @@
+﻿namespace DesafioByCoders.Api.Transactions;
+
+public static class TransactionTypeMetadata
+{
+    public static bool IsEntry(this TransactionType transactionType)
+    {
+        return transactionType switch
+        {
+            TransactionType.Boleto => false,
+            TransactionType.Financing => false,
+            TransactionType.Rent => false,
+            _ => true
+        };
+    }
+
+    public static int Sign(this TransactionType t) => t.IsEntry() ? 1 : -1;
+}
