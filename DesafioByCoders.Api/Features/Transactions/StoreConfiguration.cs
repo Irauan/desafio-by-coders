ec14e7d0e022655b66eda0d9d2a79b174b984502
Irauan Lage <irauan.lage@gmail.com>
2025-11-02 15:09:36 -0300

    refactor: Restructure configuration for entities
    Move StoreConfiguration and TransactionConfiguration to Transactions namespace and add column name mappings for clarity and alignment. Enhances code organization and database schema consistency.

--- DesafioByCoders.Api/Features/Transactions/StoreConfiguration.cs
+++ DesafioByCoders.Api/Features/Transactions/StoreConfiguration.cs
@@ -1,19 +1,26 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace DesafioByCoders.Api.Features;
+namespace DesafioByCoders.Api.Features.Transactions;
 
 internal sealed class StoreConfiguration : IEntityTypeConfiguration<Store>
 {
     public void Configure(EntityTypeBuilder<Store> builder)
     {
+        builder.ToTable("stores");
+
         builder.HasKey(x => x.Id);
 
+        builder.Property(x => x.Id)
+               .HasColumnName("id");
+
         builder.Property(x => x.Name)
+               .HasColumnName("name")
                .HasMaxLength(100)
                .IsRequired();
 
         builder.Property(x => x.Owner)
+               .HasColumnName("owner")
                .HasMaxLength(100)
                .IsRequired();
 
--- DesafioByCoders.Api/Features/Transactions/TransactionConfiguration.cs
+++ DesafioByCoders.Api/Features/Transactions/TransactionConfiguration.cs
@@ -1,39 +1,51 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace DesafioByCoders.Api.Features;
+namespace DesafioByCoders.Api.Features.Transactions;
 
 internal sealed class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
 {
     public void Configure(EntityTypeBuilder<Transaction> builder)
     {
+        builder.ToTable("transactions");
+
         builder.HasKey(x => x.Id);
 
+        builder.Property(x => x.Id)
+               .HasColumnName("id");
+
         builder.Property(x => x.Type)
+               .HasColumnName("type")
                .HasConversion<int>()
                .IsRequired();
 
         builder.Property(x => x.Amount)
+               .HasColumnName("amount")
                .HasColumnType("numeric(18,2)")
                .IsRequired();
 
         builder.Property(x => x.SignedAmount)
+               .HasColumnName("signed_amount")
                .HasColumnType("numeric(18,2)")
                .IsRequired();
 
         builder.Property(x => x.OccurredAtUtc)
+               .HasColumnName("occurred_at_utc")
                .HasColumnType("timestamp with time zone")
                .IsRequired();
 
         builder.Property(x => x.Cpf)
+               .HasColumnName("cpf")
                .HasMaxLength(11)
                .IsRequired();
 
         builder.Property(x => x.Card)
+               .HasColumnName("card")
                .HasMaxLength(12)
                .IsRequired();
 
         builder.Property(x => x.RawLineHash)
+               .HasColumnName("raw_line_hash")
                .HasMaxLength(64)
                .IsRequired();
 
@@ -41,6 +53,7 @@ internal sealed class TransactionConfiguration : IEntityTypeConfiguration<Transa
                .IsUnique();
 
         builder.Property(x => x.StoreId)
+               .HasColumnName("store_id")
                .IsRequired();
 
         builder.HasOne(x => x.Store)
