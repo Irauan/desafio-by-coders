6a425c7410e5221c6b44ddddb9bce79b3dc1ec5a
Irauan Lage <irauan.lage@gmail.com>
2025-11-03 20:24:36 -0300

    feat: Add proxy configuration for Angular development server
    Set up a proxy configuration to route API requests during development, simplifying integration with the backend. Adjusted port settings and updated editor configuration for consistency.

---
+++ DesafioByCoders.Ui.Web/proxy.conf.js
@@ -0,0 +1,14 @@
+﻿console.debug("Target api:", process.env["services__desafiobycoders-api__https__0"]);
+
+const target = process.env["services__desafiobycoders-api__https__0"];
+
+const PROXY_CONFIG = {
+    "/api": {
+        target: target,
+        secure: false,
+        changeOrigin: true,
+        logLevel: "debug"
+    }
+}
+
+module.exports = PROXY_CONFIG;
