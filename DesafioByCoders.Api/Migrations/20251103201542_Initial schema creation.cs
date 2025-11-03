161336985a26052717df1fe6c6a00a602351caf9
Irauan Lage <irauan.lage@gmail.com>
2025-11-03 17:20:11 -0300

    feat: Add initial database schema with migrations
    Introduce the initial database schema, including tables for stores and transactions, along with associated attributes and constraints. Supports the foundation for database operations and future feature development.

---
+++ DesafioByCoders.Api/Migrations/20251103201542_Initial schema creation.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DesafioByCoders.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialschemacreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "stores",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    owner = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_stores", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "transactions",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    store_id = table.Column<int>(type: "integer", nullable: false),
+                    type = table.Column<int>(type: "integer", nullable: false),
+                    amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    signed_amount = table.Column<decimal>(type: "numeric(18,2)", nullable: false),
+                    occurred_at_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    cpf = table.Column<string>(type: "character varying(11)", maxLength: 11, nullable: false),
+                    card = table.Column<string>(type: "character varying(12)", maxLength: 12, nullable: false),
+                    raw_line_hash = table.Column<string>(type: "character varying(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_transactions", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_transactions_stores_store_id",
+                        column: x => x.store_id,
+                        principalTable: "stores",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_stores_name",
+                table: "stores",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_raw_line_hash",
+                table: "transactions",
+                column: "raw_line_hash",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_store_id",
+                table: "transactions",
+                column: "store_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "transactions");
+
+            migrationBuilder.DropTable(
+                name: "stores");
+        }
+    }
+}
